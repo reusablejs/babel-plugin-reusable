4ce7f5bc98466f94d33fd7ebf0c62828e815c474
Maayan Glikser <maayan@glikm.com>
2019-04-10 21:22:05 +0300

    add test to not transform if import is not from reusable

---
+++ test/fixtures/do-nothing-not-reusable/output.js
@@ -0,0 +1,13 @@
+import { reuseState, reuseMemo as memo } from "dudu";
+
+const counter = () => {
+  const [count, setCount] = reuseState(0);
+  const nullValue = reuseState();
+  const [value, setValue] = reuseState(0, 'debugName');
+  const likes = memo(() => {
+    return count + 1000;
+  }, [count]);
+  const retweets = memo(() => {
+    return 1000;
+  });
+};
